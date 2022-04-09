df9b3f4674a9103a42fc88d836d2160a5ac7f4a2
Ariana <arianacabral57@gmail.com>
2022-04-08 23:09:00 -0300

    Add files 📑

---
+++ index.js
@@ -0,0 +1,24 @@
+const textElement = document.getElementById('text');
+
+const text = "Hello! I'm Ariana!";
+
+let idx = 1;
+
+let speed = 100;
+
+writeText();
+
+function writeText() {
+    
+    textElement.innerText = text.slice(0, idx);
+
+    idx++;
+
+    if(idx > text.length) {
+        idx = 1;
+    }
+
+    setTimeout(writeText, speed);
+};
+
+speedShow.addEventListener('input', (e) => speed = 200 / e.target.value);
